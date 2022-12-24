4fabe04c4daf490bef48021d49e5fb0b615db857
DonzerHD <thomas.lemay59@outlook.fr>
2022-12-24 11:22:49 +0100

    Panier fait

--- src/main/java/com/config/gamer/config/gamer/metier/Service/ProduitService.java
+++ src/main/java/com/config/gamer/config/gamer/metier/Service/ProduitService.java
@@ -1,6 +1,9 @@
 package com.config.gamer.config.gamer.metier.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 import com.config.gamer.config.gamer.donnees.DO.Produit;
 import com.config.gamer.config.gamer.metier.DTO.ProduitDTO;
@@ -12,4 +15,5 @@ public interface ProduitService {
 	
 	List<ProduitDTO> findAll();
 	void createProduit(ProduitDTO produitDTO);
+	Produit getById(long id);
 }
