74285246daea8c24ae3e59e86c8981489bbe9bb6
Thomas.l59 <113193305+DonzerHD@users.noreply.github.com>
2022-12-25 19:18:48 +0100

    Update ProduitDAO.java

--- src/main/java/com/config/gamer/config/gamer/donnees/DAO/ProduitDAO.java
+++ src/main/java/com/config/gamer/config/gamer/donnees/DAO/ProduitDAO.java
@@ -1,7 +1,6 @@
 package com.config.gamer.config.gamer.donnees.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.config.gamer.config.gamer.donnees.DO.Produit;
 
 public interface ProduitDAO extends JpaRepository<Produit, Long> {
